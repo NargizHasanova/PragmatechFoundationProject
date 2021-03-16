41c6ea34b41ad63ea39203adec9a162928f51326
Nargiz Hasanova <nargiz.gasanova.1996@mail.ru>
2021-03-16 14:38:33 -0700

    js practice,functions and object

--- practice/js/app.js
+++ practice/js/app.js
@@ -64,15 +64,76 @@ numbers(10)
 a=3
 b=5
 c=a+b
-console.log(a+'+'+b+'='+c)
+console.log(a+'+'+b+'='+c);
 
 //bir nece deyeri bir deyisenin icine yazmaq
 let isim='nara',soyisim='hesenova',age=24
-console.log(age)
+console.log(age);
 
 let reqem=chislo=numberr=33
-console.log(chislo)
+console.log(chislo);
 
+// sade bir funksiya
+function deneme(){
+	document.write("HELLO WORLD! <br>")
+}
+deneme();
+
+
+// bir deyisene verdiyimiz funksiya(burda artiq funksiyanin adi deyisendedi,funksiyaya ad vermirik)
+let hesab = function(){
+	document.write("HELLO MAN!<br>");
+}
+hesab();      //hesab-i cagirmagin bir yolu budu
+
+/*let hesab = function(){
+ 	document.write("HELLO MAN!<br>")
+}()*/        //hesab-i cagirmagin diger yoluda budu
+
+//deyisene funksiya vermeyimizin basqa bir varianti
+function greet(){
+	let woman="HELLO WOMAN <br>"
+    return woman;
+}
+var example = greet();
+document.write(example);
+
+//avtomatik cagirilan funksiya
+(function test(){
+	document.write("Avtomatik funksiya <br>")
+})();
+
+
+let infoBuro={
+	ad:"nergiz",
+	soyad:'hesenova',
+	yas:24,
+	tehsil:'ATU',
+	ixtisas:"frontend developer <br>",
+	evli:false,
+	milliyet:"azerbaycanli"
+}
+document.write(infoBuro.ixtisas);
+
+
+let info = new Object();
+	info.ad="nergiz";
+	info.soyad='hesenova ';
+	info.yas=24;
+	info.tehsil='ATU';
+	info.ixtisas="frontend developer";
+	info.evli=false;
+	info.milliyet="azerbaycanli";
+document.write(info.soyad)
+
+
+
+function ornek(){
+  this.ad="nergiz";
+  this.soyad='hesenova'
+}
+let bilgiler=new ornek();
+document.write(bilgiler.ad)
 
 
 
