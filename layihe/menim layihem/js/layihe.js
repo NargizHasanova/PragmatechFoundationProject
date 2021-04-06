c104222947ec12446eeb7db23bcfbb19794a1ce8
Nargiz Hasanova <nargiz.gasanova.1996@mail.ru>
2021-04-06 08:03:41 -0700

    layihe uzerinde isledim

--- layihe/menim layihem/js/layihe.js
+++ layihe/menim layihem/js/layihe.js
@@ -1,3 +1,32 @@
-const name="nargiz"
+let rightBtn=document.querySelector('#rightBtn');
+let leftBtn=document.querySelector('#leftBtn'); 
+let carouselSlide=document.querySelectorAll('.carousel-slide');
+let carouselImages=document.querySelectorAll('.carousel-slide img')
 
-console.log(name)
+let counter=0;
+let size=carouselImages[0].clientWidth; //1355
+
+rightBtn.addEventListener('click',function(){
+    counter++;
+    if (counter==carouselImages.length) {
+    	counter=0;
+    }
+	carouselSlide.forEach( function(element, index) {
+		element.style.transform='translateX('+(-size*counter)+'px)';
+	    element.style.transition="transform 0.33s ease-in-out"; 
+	});
+});
+
+
+
+leftBtn.addEventListener('click',function(){
+	counter--;
+	if (counter<0) {
+		counter=carouselImages.length-1;	
+	}
+	carouselSlide.forEach( function(element, index) {
+		element.style.transform='translateX('+(-size*counter)+'px)';
+	    element.style.transition="transform 0.33s ease-in-out"; 
+	});
+		
+})
