212cd9c63c00678318f66a82241c458a95d68c32
Nargiz Hasanova <nargiz.gasanova.1996@mail.ru>
2021-04-19 20:54:15 +0500

    'Javascript ne vaxtdan proqramlasdirma dili olub?' cumlesindeki soz sayini tapin

---
+++ tasks/python/task3.py
@@ -0,0 +1,11 @@
+# Task 3. 'Javascript ne vaxtdan proqramlasdirma dili olub?' cumlesinin soz sayini tapin
+
+my_sentence = 'Javascript ne vaxtdan proqramlasdirma dili olub?'
+word_list=my_sentence.split()
+number_of_words=len(word_list)
+print(number_of_words)
+    
+
+
+
+
